6cf674153b3521a5df3f8133e0b3bb181ee49bd4
wilbertmarroquin <wilbertmarroquin1996@gmail.com>
2018-02-08 18:34:54 -0500

    item_model

--- src/item/item_/models.py
+++ src/item/item_/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+# Create your models here.
+class ExistenceType(models.Model):
+	existence_type_id = models.AutoField(primary_key = True)
+	code              = models.CharField(max_length = 45)
+	name              = models.CharField(max_length = 45)
+	def __str__(self):
+		return self.name
 
 class ItemCategorie(models.Model):
+	item_category_id = models.AutoField(primary_key = True)
+	category_name    = models.CharField(max_length = 45)
+	has_kardex       = models.IntegerField()
+	def __str__(self):
+		return self.category_name
 
-    item_id = models.AutoField(primary_key=True)
-    item_name = models.CharField(max_length=45)
-    has_kardex = models.IntegerField()
-
-    def __str__(self):
-        return self.item_name
+class Item(models.Model):
+	item_id        = models.AutoField(primary_key = True)
+	item_name      = models.CharField(max_length = 45)
+	description    = models.CharField(max_length = 48)
+	category_id    = models.ForeignKey(ItemCategorie, on_delete = models.CASCADE)
+	existence_type = models.ForeignKey(ExistenceType,on_delete = models.CASCADE) 
+	def __str__(self):
+		return self.item_name
